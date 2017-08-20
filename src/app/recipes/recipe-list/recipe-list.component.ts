27505f437543277b36f27d1cdb6aedf8843badce
rocky-bgta <rocky.bgta@gmail.com>
2017-08-20 08:45:47 +0600

    Details component implemented

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe";
 
 @Component({
@@ -11,7 +11,12 @@ export class RecipeListComponent implements OnInit {
     recipe = new Recipe("Dummy", "Dummy","https://www.worldwidedisplays.co.uk/media/catalog/product/cache/1/small_image/295x295/9df78eab33525d08d6e5fb8d27136e95/6/k/6k2033_6k20722.jpg" )
   constructor() { }
 
+  @Output()
+  recipeSelected = new EventEmitter<Recipe>();
   ngOnInit() {
   }
 
+  onSelected(recipe: Recipe){
+    this.recipeSelected.emit(recipe);
+  }
 }
