1bda63aa171990a12481c16572d3ab474d89d780
rocky-bgta <rocky.bgta@gmail.com>
2017-08-22 22:41:12 +0600

    Implement edit functionality

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -18,4 +18,9 @@ export class ShoppingListService {
       this.items.push(item);
   }
 
+  editItem(oldItem: Ingredient, newItem: Ingredient){
+      this.items[this.items.indexOf(oldItem)] = newItem;
+  }
+
+
 }
