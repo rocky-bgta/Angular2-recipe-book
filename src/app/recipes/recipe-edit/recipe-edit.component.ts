8502d15578760e82af760c5ab60033221d89a5bc
rocky-bgta <rocky.bgta@gmail.com>
2017-08-23 12:44:28 +0600

    do rest of work from home

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,14 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {RecipeService} from "../recipe.service";
+import {Subscription} from "rxjs";
+import {Recipe} from "../recipe";
+import {FormArray} from "@angular/forms";
 
 @Component({
   selector: 'rb-recipe-edit',
   templateUrl: './recipe-edit.component.html'
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
+  private subsciption: Subscription;
+  private recipeIndex: number;
+  private recipe: Recipe;
+  private isName = true;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) { }
 
-  ngOnInit() {
+  ngOnInit(){
+    let isNew = true;
+    this.subsciption = this.route.params.subscribe(
+      (params: any)=> {
+
+        if(params.hasOwnProperty('id')){
+          isNew = false;
+          this.recipeIndex = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.recipeIndex);
+        }
+        else{
+          isNew = true;
+          this.recipe = null;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.subsciption.unsubscribe();
+  }
+
+  private initForm(isNew: boolean){
+
+    let recipeName = '';
+    let recipeImageUrl = '';
+    let recipeContent = '';
+    let recipeIngredients: FormArray = new FormArray([]);
+
   }
 
 }
